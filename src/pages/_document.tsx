6dd15716883b9a2c88b18e4f9ec03e1611a89a3b
Levir Cesar <levirlemos@gmail.com>
2021-04-28 10:04:15 -0300

    update responsive 1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,20 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+          <meta name="author" content="Levir César Ribeiro Lemos"/>
+          <meta name="description" content="Levir Lemos | Website" />
+          <meta name="keywords" content="levir cesar, levir lemos, engenheiro de computação,desenvolvimento web,seo,marketing digital,programação,web design,front-end,web developer,back-end,php" />
+          <meta name="robots" content="index,follow"/>
+          <meta name="theme-color" content="#483079"/> 
+          <meta name="format-detection" content="telephone=no"/>
+          <meta property="og:title" content="Levir Lemos | Website"/>
+          <meta property="og:site_name" content="Levir Lemos"/>
+          <meta property="og:description" content="Levir Lemos | Website"/>
+          <meta property="og:url" content="https://portifolio-next-omega.vercel.app/"/>
+          <meta property="og:image" content="https://portifolio-next-omega.vercel.app/favicon.png"/>
+          <meta property="og:image:type" content="image/png"/>
+
         </Head>
         <body>
           <Main/>
